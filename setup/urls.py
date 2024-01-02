e67c9f5ffec76b4ec07e130775709a430d5362b0
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-02 16:22:18 -0300

    Configurando urls

--- setup/urls.py
+++ setup/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register("aluno", AlunosViewSet, basename="Alunos")
+router.register("curso", CursosViewSet, basename="Cursos")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("alunos/", alunos),
+    path("", include(router.urls)),
 ]
